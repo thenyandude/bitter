2d17be2df91bf66af8aa682855ff7141fa600ae4
Nyan <nyandude22@gmail.com>
2024-04-08 09:46:17 +0200

    Updating app to use Guide

--- bitter/src/App.js
+++ bitter/src/App.js
@@ -11,14 +11,28 @@ import Guide from './components/Guide';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
+
+  const [firstVisit, setFirstVisit] = useState(false);
+
+  useEffect(() => {
+    // Check if it's the user's first visit
+    const isFirstVisit = localStorage.getItem('firstVisit') === null;
+    setFirstVisit(isFirstVisit);
+
+    // Set 'firstVisit' in local storage
+    if (isFirstVisit) {
+      localStorage.setItem('firstVisit', 'no');
+    }
+  }, []);
+
+
   return (
     <AuthProvider>
       <div className="App">
         <Router>
           <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={firstVisit ? <Navigate to="/guide" /> : <Home />} />            <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<UserHome userId={localStorage.getItem('userId')} />} />
             <Route path="/guide" element={<Guide />} />
