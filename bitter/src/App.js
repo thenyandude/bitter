c14893b6fba220a1be50b0641c8089eae1efe0f4
Nyan <nyandude22@gmail.com>
2024-04-08 09:35:51 +0200

    Updating app to use Guide

--- bitter/src/App.js
+++ bitter/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/App.css';
 import Header from './components/Header';
@@ -7,17 +7,30 @@ import Login from './components/LogIn';
 import UserHome from './components/UserHome';
 import Home from './components/Home';
 import FindPosts from './components/FindPosts';
-
+import Guide from './components/Guide';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
+  const [firstVisit, setFirstVisit] = useState(false);
+
+  useEffect(() => {
+    // Check if it's the first visit
+    const isFirstVisit = localStorage.getItem('firstVisit') === null;
+    setFirstVisit(isFirstVisit);
+
+    // Set 'firstVisit' in local storage
+    if (isFirstVisit) {
+      localStorage.setItem('firstVisit', 'no');
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <div className="App">
         <Router>
           <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={firstVisit ? <Guide /> : <Home />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<UserHome userId={localStorage.getItem('userId')} />} />
